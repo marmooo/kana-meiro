9b8c69244e0b22e347e0e3f1d96e0ded6ad56242
marmooo <marmooo@users.noreply.github.com>
2021-08-05 07:09:08 +0900

    deno lint

--- src/sw.js
+++ src/sw.js
@@ -1,48 +1,47 @@
-var CACHE_NAME = '2021-06-24 07:06';
+var CACHE_NAME = "2021-08-05 07:05";
 var urlsToCache = [
-  '/kana-meiro/',
-  '/kana-meiro/index.js',
-  '/kana-meiro/mp3/incorrect1.mp3',
-  '/kana-meiro/mp3/cat.mp3',
-  '/kana-meiro/mp3/correct3.mp3',
-  '/kana-meiro/favicon/favicon-48x48.png',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+  "/kana-meiro/",
+  "/kana-meiro/index.js",
+  "/kana-meiro/mp3/incorrect1.mp3",
+  "/kana-meiro/mp3/cat.mp3",
+  "/kana-meiro/mp3/correct3.mp3",
+  "/kana-meiro/favicon/favicon-48x48.png",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
     caches
-    .open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      }),
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
+      }),
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener("activate", function (event) {
   var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
-    })
+    }),
   );
 });
